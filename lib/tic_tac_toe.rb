016b57dc346c949251009c3740603b3c014b6204
Nathan Glick <nateleighglick@hotmail.com>
2020-10-21 20:15:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,6 +48,7 @@ def turn(board)
   user_input = gets.strip.to_i
   index = input_to_index(user_input)
   if valid_move?(board, index)
+    
     move(board, index, token)
     display_board(board)
 else 
